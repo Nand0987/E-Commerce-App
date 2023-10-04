0dfc029453f848e5d79ee596ee1f69937e262f6d
Nand0987 <103673462+Nand0987@users.noreply.github.com>
2023-10-04 14:45:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path"
 import { fileURLToPath } from "url";
 const __filename=fileURLToPath(import.meta.url)
 const __dirname=path.dirname(__filename)
