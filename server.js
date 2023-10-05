0f83ed1b7c982457f2798bbd66c25dd7c34df628
Nand0987 <103673462+Nand0987@users.noreply.github.com>
2023-10-05 18:31:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-// import path from "path"
-// import { fileURLToPath } from "url";
-// const __filename=fileURLToPath(import.meta.url)
-// const __dirname=path.dirname(__filename)
+import path from "path"
+import { fileURLToPath } from "url";
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
 
 //configure env
 dotenv.config();
@@ -22,11 +22,11 @@ connectDB();
 const app = express();
 
 //middleware
-// app.use(express.static(path.join(__dirname,'./client/build/')))
+app.use(express.static(path.join(__dirname,'./client/build/')))
 
-// app.use('*',function(req,res){
-//   res.sendFile(path.join(__dirname,'./client/build/index.html'))
-// })
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 //middelwares
 app.use(cors());
