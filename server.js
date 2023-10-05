dfdbe1aa9bca6aa048a80c32dbeb53d4d74828a6
Nand0987 <103673462+Nand0987@users.noreply.github.com>
2023-10-05 22:38:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,49 +13,46 @@ import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename); 
 
-//rest object
-const app = express();    
+// Create an Express app
+const app = express();
 
-// Serve static files from the "build" directory`
-
-const buildPath = join(__dirname, '/client/build');
-console.log(buildPath);
-app.use(express.static(buildPath));
-
-app.get('*', (req, res) => {
-  res.sendFile(join(buildPath, '/index.html'));
-});
-
-//configure env
+// Configure environment variables
 dotenv.config();
 
-//databse config
+// Connect to the database
 connectDB();
 
-
-
-//middelwares
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-//routes
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
+// REST API
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-//PORT
+// Serve static files from the "build" directory
+const buildPath = join(__dirname, '/client/build');
+console.log(buildPath);
+app.use(express.static(buildPath));
+
+// Catch-all route for React app
+app.get('*', (req, res) => {
+  res.sendFile(join(buildPath, '/index.html'));
+});
+
+// Set the port
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// Start the server
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
   );
 });
