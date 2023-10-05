374fb02d707268503c15b5fd65ac1bb685bd882d
Nand0987 <103673462+Nand0987@users.noreply.github.com>
2023-10-05 22:35:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,46 +13,49 @@ import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename); 
 
-// Create an Express app
-const app = express();
+//rest object
+const app = express();    
 
-// Configure environment variables
+// Serve static files from the "build" directory`
+
+const buildPath = join(__dirname, '/client/build');
+console.log(buildPath);
+app.use(express.static(buildPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(buildPath, '/index.html'));
+});
+
+//configure env
 dotenv.config();
 
-// Connect to the database
+//databse config
 connectDB();
 
-// Middleware
+
+
+//middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// Routes
+//routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// REST API
+//rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-// Serve static files from the "build" directory
-const buildPath = join(__dirname, 'client/build');
-console.log(buildPath);
-app.use(express.static(buildPath));
-
-// Catch-all route for React app
-app.get('*', (req, res) => {
-  res.sendFile(join(buildPath, 'index.html'));
-});
-
-// Set the port
+//PORT
 const PORT = process.env.PORT || 8080;
 
-// Start the server
+//run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
   );
 });
